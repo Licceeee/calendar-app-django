f1573d28b36fe6568d594b4f2d3796049fa0220b
Alicia Schonefeld <schonefeld.dev@gmail.com>
2022-04-28 00:07:47 +0200

    add routine to admin

--- calendarapp/admin.py
+++ calendarapp/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
-from calendarapp.models import Event, EventMember
+from calendarapp.models import (Event, EventMember, Routine)
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     model = Event
     list_display = [
-        "id",
         "title",
         "user",
         "is_active",
         "is_deleted",
         "created_at",
         "updated_at",
+        "id",
     ]
     list_filter = ["is_active", "is_deleted"]
     search_fields = ["title"]
@@ -21,5 +21,12 @@ class EventAdmin(admin.ModelAdmin):
 @admin.register(EventMember)
 class EventMemberAdmin(admin.ModelAdmin):
     model = EventMember
-    list_display = ["id", "event", "user", "created_at", "updated_at"]
+    list_display = ["event", "user", "created_at", "updated_at", "id"]
     list_filter = ["event"]
+
+
+@admin.register(Routine)
+class RoutineAdmin(admin.ModelAdmin):
+    model = Routine
+    list_display = ["id", "name", "user", "created_at", "updated_at", "id"]
+    list_filter = ["name"]
