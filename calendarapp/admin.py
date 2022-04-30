54616d6741ff1a7cb9091b6b73ef888cc6246cb0
Alicia Schonefeld <schonefeld.dev@gmail.com>
2022-04-30 16:29:54 +0200

    temporary solution err with get_search_results in admin routineadmin class

--- calendarapp/admin.py
+++ calendarapp/admin.py
@@ -20,20 +20,24 @@ class EventAdmin(admin.ModelAdmin):
 @admin.register(EventMember)
 class EventMemberAdmin(admin.ModelAdmin):
     list_display = ["event", "user", "created_at", "updated_at", "id"]
-    list_filter = ["event"]
+    list_filter = ("event",)
 
 
 @admin.register(Routine)
 class RoutineAdmin(admin.ModelAdmin):
     list_display = ["name", "user", "created_at", "updated_at", "id"]
-    list_filter = ["name"]
+    list_filter = ("name",)
     search_fields = ["name"]
 
     # filters routines by logged in user // for autocomplete - not for dropdown
+    # ! gives ERROR in class ChangeList. Path:
+    # venv/lib/python#/site-packages/django/contrib/admin/views/main.py:485
+    # temporary solution: comment out: remove duplicates, from line 485 to 489
+
     def get_search_results(self, request, queryset, search_term):
-        results = super().get_search_results(request, queryset, search_term)
+        res = super().get_search_results(request, queryset, search_term)
         queryset = self.model.objects.filter(user=request.user)
-        return queryset, results
+        return queryset, res
 
 
 @admin.register(DailyRoutine)
